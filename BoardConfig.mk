8ba90b6673388d0854657f56d5709b4160fdd56a
Gabriele M <moto.falcon.git@gmail.com>
2016-11-21 15:08:28 +0100

    Add CdmaCarrierConfig app
    This app allows CDMA users to define the name and the numeric of
    their carrier.
    
    The app sets persist.operator.numeric and persist.operator.alpha
    which are read by init at boot to set ro.cdma.home.operator.numeric
    and ro.cdma.home.operator.alpha respectively.
    
    Change-Id: I5538fda19c7b978bbec9a5b525ac1a56d800b4be

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,8 @@ TARGET_OTA_ASSERT_DEVICE := j7popltespr
 # Kernel
 TARGET_KERNEL_CONFIG := j7popltespr_defconfig
 
+BOARD_SEPOLICY_DIRS += device/samsung/j7popltespr/sepolicy
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_j7popltespr
 TARGET_RECOVERY_DEVICE_MODULES := libinit_j7popltespr
