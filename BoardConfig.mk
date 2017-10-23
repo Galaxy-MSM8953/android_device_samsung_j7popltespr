46e0932ce80f5ff58517664f2e7a36ff66f0a5a7
minz1 <emerytang@gmail.com>
2017-10-22 22:43:11 -0500

    j7popltespr: correct kernel config name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@ include device/samsung/j7poplte-common/BoardConfigCommon.mk
 TARGET_OTA_ASSERT_DEVICE := j7popltespr
 
 # Kernel
-TARGET_KERNEL_VARIANT_CONFIG := msm8953_sec_j7poplte_usa_spr_defconfig
+TARGET_KERNEL_CONFIG := lineageos_j7popltespr_defconfig
 
 # Partition sizes
 BOARD_SYSTEMIMAGE_PARTITION_SIZE    := 36612931584
