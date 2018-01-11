25bab8fd4e9f4a580a04a09c4f44224203043787
minz1 <emerytang@gmail.com>
2018-01-11 13:34:13 -0600

    j7popltespr: rename defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@ include device/samsung/j7poplte-common/BoardConfigCommon.mk
 TARGET_OTA_ASSERT_DEVICE := j7popltespr
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineageos_j7popltespr_defconfig
+TARGET_KERNEL_CONFIG := j7popltespr_defconfig
 
 # Partition sizes
 BOARD_SYSTEMIMAGE_PARTITION_SIZE    := 3674210304
