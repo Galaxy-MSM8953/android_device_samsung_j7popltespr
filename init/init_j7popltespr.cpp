08580a4dc39cb2b70aab679e7cae980e2f526438
deadman96385 <seanhoyt963@gmail.com>
2018-07-02 20:17:19 -0500

    Set more configs in init
    Change-Id: I7fc783da31c3b2ec22b83bf00f6cca5a9b9bab3f

--- init/init_j7popltespr.cpp
+++ init/init_j7popltespr.cpp
@@ -39,6 +39,34 @@
 
 #include "init_msm8953.h"
 
+void set_rild_libpath(char const *variant)
+{
+    std::string libpath("/system/vendor/lib/libsec-ril.");
+    libpath += variant;
+    libpath += ".so";
+
+    property_override("rild.libpath", libpath.c_str());
+}
+
+void cdma_properties(char const *operator_alpha,
+        char const *operator_numeric,
+        char const *rild_lib_variant)
+{
+    /* Dynamic CDMA Properties */
+    property_set("ro.cdma.home.operator.alpha", operator_alpha);
+    property_set("ro.cdma.home.operator.numeric", operator_numeric);
+    set_rild_libpath(rild_lib_variant);
+
+    /* Static CDMA Properties */
+    property_set("ro.telephony.default_cdma_sub", "1");
+    property_set("telephony.lteOnCdmaDevice", "1");
+    property_set("gsm.operator.numeric", "310000");
+    property_set("persist.eons.enabled", "true");
+    property_override("ro.build.fingerprint", "samsung/j7popltespr/j7popltespr:7.0/NRD90M/J727PVPU3ARC2:user/release-keys");
+    property_override("ro.build.description", "j7popltespr-user 7.0 NRD90M J727PVPU3ARC2 release-keys");
+    property_override("ro.product.model", "SM-J727P");
+}
+
 void init_target_properties()
 {
     std::string platform = property_get("ro.board.platform");
@@ -52,18 +80,33 @@ void init_target_properties()
         property_set("ro.com.google.clientidbase.ms", "android-boost-us");
         property_set("ro.com.google.clientidbase.am", "android-boost-us");
         property_set("ro.com.google.clientidbase.yt", "android-boost-us");
+        cdma_properties("Boost Mobile", "310000", "spr");
     } else if (carrier == "VMU") {
-        property_set("ro.cdma.home.operator.alpha", "Virgin Mobile");
         property_set("ro.com.google.clientidbase.ms", "android-virgin-us");
         property_set("ro.com.google.clientidbase.am", "android-virgin-us");
         property_set("ro.com.google.clientidbase.yt", "android-virgin-us");
+        cdma_properties("Virgin Mobile", "310000", "spr");
     } else if (carrier == "SPR") {
-        property_set("ro.cdma.home.operator.alpha", "Sprint");
         property_set("ro.com.google.clientidbase.ms", "android-sprint-us");
         property_set("ro.com.google.clientidbase.am", "android-sprint-us");
         property_set("ro.com.google.clientidbase.yt", "android-sprint-us");
+        cdma_properties("Sprint", "310000", "spr");
     } else if (carrier == "XAS") {
-        property_set("ro.cdma.home.operator.alpha", "Chameleon");
+        property_set("ro.com.google.clientidbase.ms", "android-sprint-mvno-us");
+        property_set("ro.com.google.clientidbase.am", "android-sprint-mvno-us");
+        property_set("ro.com.google.clientidbase.yt", "android-sprint-mvno-us");
+        cdma_properties("Chameleon", "310000", "spr");
+    } else if (carrier == "USC") {
+        property_set("ro.com.google.clientidbase.ms", "android-sprint-us");
+        property_set("ro.com.google.clientidbase.am", "android-sprint-us");
+        property_set("ro.com.google.clientidbase.yt", "android-sprint-us");
+        property_set("ro.cdma.home.operator.numeric", "311580");
+        property_set("gsm.operator.numeric", "311580");
+        property_set("persist.eons.enabled", "false");
+        property_override("ro.build.fingerprint", "samsung/j7poplteusc/j7poplteusc:7.0/NRD90M/J727R4TYS1ARC1:user/release-keys");
+        property_override("ro.build.description", "j7poplteusc-user 7.0 NRD90M J727R4TYS1ARC1 release-keys");
+        property_override("ro.product.model", "SM-J727PR4");
+        set_rild_libpath("usc");
     } else {
         ERROR("Unknown mobile carrier");
     }
