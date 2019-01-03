2d9d1c7afb1e4993b4b76b2b4bd1ae0472ed142d
minz1 <emerytang@gmail.com>
2019-01-02 18:42:10 -0600

    j7popltespr: initial encryption support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,10 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8953
 TARGET_NO_BOOTLOADER := true
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := device/samsung/j7popltespr/cryptfs_hw
+
 # Kernel
 TARGET_KERNEL_ARCH := arm64
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
@@ -64,3 +68,4 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone1/temp
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_INCLUDE_CRYPTO := true
