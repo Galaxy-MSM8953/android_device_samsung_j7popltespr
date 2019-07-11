9c26168414011715286aa1d3ab4f01f746df1571
minz1 <emerytang@gmail.com>
2019-07-10 21:33:39 -0500

    j7popltespr: don't build power.qcom
    * It is unnecessary, and wasn't even being built anyway

--- device.mk
+++ device.mk
@@ -271,9 +271,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
     android.hardware.power@1.0-service
 
-PRODUCT_PACKAGES += \
-    power.qcom
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
