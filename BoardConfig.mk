11201ed36e45191a8772f68008a25e7cc40c151c
minz1 <emerytang@gmail.com>
2019-07-23 23:41:37 -0500

    j7popltespr: remove deprecated TARGET_USES_64_BIT_BINDER

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,8 +41,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
-TARGET_USES_64_BIT_BINDER := true
-
 # Kernel
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
