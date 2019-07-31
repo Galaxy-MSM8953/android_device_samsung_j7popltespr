afde2ae03d738a7c04e6f1fd7b2a83a2728f1b83
minz1 <emerytang@gmail.com>
2019-07-30 19:20:25 -0500

    j7popltespr: build libcurl

--- device.mk
+++ device.mk
@@ -177,6 +177,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GPS
+PRODUCT_PACKAGES += \
+    libcurl
+
 # Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
