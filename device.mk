cf2944df60669d7a15a41ee69b17dda030066d16
minz1 <emerytang@gmail.com>
2019-07-31 02:52:58 -0500

    j7popltespr: add samsung camera provider service

--- device.mk
+++ device.mk
@@ -127,8 +127,8 @@ PRODUCT_PACKAGES += \
      Snap
 
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.3-impl \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-impl \
     libjpega \
     libshims_camera 
 
