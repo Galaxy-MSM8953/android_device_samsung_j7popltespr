7c36cac9dfd4a6b0a04809bf63175e9ae36fa7a3
minz1 <emerytang@gmail.com>
2019-08-15 12:16:29 -0500

    j7popltespr: camera: add missing parameters

--- camera/CameraParameters.cpp
+++ camera/CameraParameters.cpp
@@ -18,8 +18,17 @@
 
 namespace android {
 
-const char CameraParameters::KEY_OIS_SUPPORTED[] = "ois_supported";
+const char CameraParameters::KEY_OIS[] = "ois";
+const char CameraParameters::KEY_OIS_SUPPORTED[] = "ois-supported";
+const char CameraParameters::KEY_SUPPORTED_OIS_MODES[] = "ois-modes";
 const char CameraParameters::KEY_SUPPORTED_EFFECT_PREVIEW_FPS_RANGE[] = "effect-available-fps-values";
+const char CameraParameters::OIS_CENTERING[] = "ois-centering";
+const char CameraParameters::OIS_OFF[] = "off";
+const char CameraParameters::OIS_ON_SINE_X[] = "ois-on-sin-x";
+const char CameraParameters::OIS_ON_SINE_Y[] = "ois-on-sin-y";
+const char CameraParameters::OIS_ON_STILL[] = "ois-on-still";
+const char CameraParameters::OIS_ON_VIDEO[] = "ois-on-video";
+const char CameraParameters::OIS_ON_ZOOM[] = "ois-on-zoom";
 const char CameraParameters::PIXEL_FORMAT_YUV420SP_NV21[] = "yuv420sp";
 int CameraParameters::getInt64(const char *key) const { return -1; }
 
